e34a7c504c8ec209059c196774626b37444fd175
Jonas Macedo <jonasmdcarvalho@gmail.com>
2019-09-19 17:00:31 -0300

    Estilização basica dos Cards.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import { Container } from './style';
+import { Container, Label } from './style';
 
 export default function Card() {
   return (
     <Container>
-
+      <header>
+        <Label color="#7159c1"/>
+      </header>
+      <p> Descrição da Bio do Card.</p>
+      <img src='https://api.adorable.io/avatars/97/abott@adorable.png' alt="Avatar"/>
       
     </Container>
   );
--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+    img{
+        width: 34px;
+        height: 34px;
+        border-radius: 7px;
+        margin-top: 5px;
+    }
+`;
+
+export const Label = styled.span`
+
 `;
