aa5222b96afedcd29c1084cc6ab95c431f700bd0
Jonas Macedo <jonasmdcarvalho@gmail.com>
2019-09-19 16:29:31 -0300

    Estilizando cards

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import {MdAdd} from 'react-icons/md';
+
+import Card  from '../Card';
+
 import { Container } from './styles';
 
 export default function List() {
@@ -11,6 +14,13 @@ export default function List() {
         <button type="button">
           <MdAdd size={24} color="#fff"/>
         </button>
+        <ul>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </ul>
       </header>
     </Container>
   );
