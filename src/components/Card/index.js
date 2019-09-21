78f3976f210c7eb373ca804060874fb384879c27
Jonas Macedo <jonasmdcarvalho@gmail.com>
2019-09-20 23:55:26 -0300

    Movendo cards nas listas.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {useDrag} from 'react-dnd';
+import {useDrag, useDrop} from 'react-dnd';
 
 import { Container, Label } from './style';
 
 export default function Card({data}) {
 
+  //Ira monitorar as movimentações dos cards.
   const [{isDragging},dragRef] = useDrag({
     item: {type: 'CARD'},
     collect: monitor =>({
       isDragging: monitor.isDragging(),
     })
+  });
+
+  const[,dropRef]=useDrop({
+    accept: 'CARD',
+    hover(item,monitor){
+      
+    }
   });
 
   return (
